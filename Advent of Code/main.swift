a6b7801787228d4298dedb975413cb285a77a6b1
Dimitar Chakarov <dimitar@dchakarov.com>
2021-12-15 21:45:49 +0000

    Part 2

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -56,4 +56,83 @@ func process(template: String, rules: [String: String]) -> String {
     return newTemplate.joined(separator: "")
 }
 
-main()
+func main2() {
+    let fileUrl = URL(fileURLWithPath: "./aoc-input")
+    guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
+
+    var lines = inputString.components(separatedBy: "\n")
+        .filter { !$0.isEmpty }
+
+    let template = lines.removeFirst()
+
+    var rules = [[String]: String]()
+
+    lines.forEach { line in
+        let ar = line.components(separatedBy: " -> ")
+        let pair = Array(ar[0]).map(String.init)
+        rules[pair] = ar[1]
+    }
+
+    var pairs = [[String]: Int]()
+    let templateArray = Array(template).map(String.init)
+    for i in 0 ..< templateArray.count - 1 {
+        let el = [templateArray[i], templateArray[i+1]]
+        if pairs.keys.contains(el) {
+            pairs[el]! += 1
+        } else {
+            pairs[el] = 1
+        }
+    }
+
+    for _ in 0 ..< 40 {
+        pairs = process2(pairs: pairs, rules: rules)
+    }
+
+    var charCounts = [String: Int]()
+    for (pair, count) in pairs {
+        if charCounts.keys.contains(pair[0]) {
+            charCounts[pair[0]]! += count
+        } else {
+            charCounts[pair[0]] = count
+        }
+        if charCounts.keys.contains(pair[1]) {
+            charCounts[pair[1]]! += count
+        } else {
+            charCounts[pair[1]] = count
+        }
+    }
+
+    var realCharCounts = charCounts.mapValues { $0 / 2 }
+    realCharCounts[templateArray.first!]! += 1
+    realCharCounts[templateArray.last!]! += 1
+
+    let result = realCharCounts.values.max()! - realCharCounts.values.min()!
+
+    print(result)
+}
+
+func process2(pairs: [[String]: Int], rules: [[String]: String]) -> [[String]: Int] {
+    var newPairs = pairs
+    for pair in pairs.keys {
+        let pairCount = pairs[pair]!
+        if pairCount > 0, let letter = rules[pair] {
+            newPairs[pair]! -= pairCount
+            let newPair1 = [pair[0], letter]
+            let newPair2 = [letter, pair[1]]
+
+            if newPairs.keys.contains(newPair1) {
+                newPairs[newPair1]! += pairCount
+            } else {
+                newPairs[newPair1] = pairCount
+            }
+            if newPairs.keys.contains(newPair2) {
+                newPairs[newPair2]! += pairCount
+            } else {
+                newPairs[newPair2] = pairCount
+            }
+        }
+    }
+    return newPairs
+}
+
+main2()
